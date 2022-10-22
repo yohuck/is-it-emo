7ca37e5222a85bb74dcee5b33b6be861e60b6c14
Eric Lake <ericallenlake@gmail.com>
2022-10-22 13:17:00 -0600

    Meter body & pointer animation

--- src/components/Meter.js
+++ src/components/Meter.js
@@ -1,15 +1,37 @@
 import { gsap } from "gsap";
 
+let toggle = false;
+
 const onClick = () => {
-    console.log('weee')
-    gsap.to(".anim", { rotation: 360, yoyo: true, ease: "bounce-out", repeat: 1})
+    if (toggle){
+        gsap.to(".pointer", { rotation: 60, ease: "bounce-out"})
+        gsap.to(".meter-body-left", { backgroundColor: 'var(--red-6)'})
+        gsap.to(".meter-body-right", { backgroundColor: 'var(--red-6)'})
+        toggle = !toggle
+    } else {
+        gsap.to(".pointer", { rotation: -60, ease: "bounce-out"})
+        gsap.to(".meter-body-left", { backgroundColor: 'var(--green-6)'})
+        gsap.to(".meter-body-right", { backgroundColor: 'var(--green-6)'})
+        toggle = !toggle
+    
+}
 }
 
 function Results() {
     return (
-        <section className="meter">
-            <button onClick={onClick} className="anim">Click</button>
-        </section>
+
+            <div className="con">
+                <section className="meter relative">
+                    <div className="meter-body-left">
+                        <div className="meter-pointer"></div>
+                    </div>
+                    <div className="meter-body-right">
+                        <div className="meter-pointer"></div>
+                    </div>
+                    <div className="pointer"></div>
+                </section>
+                <button onClick={onClick}>Click</button>
+            </div>
     );
   }
 
